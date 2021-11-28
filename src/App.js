7bcc4f7c068ae4a882d2f7f9e01957200c42b993
danielfostiak <fostiakd@gmail.com>
2021-11-28 17:02:46 +0000

    i think finale, only pdf left?

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import RangeSlider from "react-bootstrap-range-slider";
+import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
 import "./app.css";
+import { FormControl } from "react-bootstrap";
 
-const text = `Reading is becoming more and more important in the new knowledge economy and remains the most effective human activity for transforming information into knowledge. If top readers read at speeds of above 1000 words per minute (wpm) with near 85% comprehension, they only represent 1% of readers. Average readers are the majority and only reach around 200 wpm with a typical comprehension of 60%. This seems surprising since most readers, actively reading work documents, newspapers, magazines, books or the contents of a computer display are practicing daily for at least one hour. With such an intense training everyone should be close to top performances. Unfortunately, this is far from the real situation. The average reader is five times slower than the good reader. Things are even worse if we consider reading efficiency as well as speed. Compare the results of the average reader to other areas. We may imagine a sprinter practicing every day for several years on the running track and then just calmly walking for a race. We can also picture a racing driver never exceeding 30 mph or a pianist playing every day of the week for 20 years and only able to play music like a beginner. Unfortunately, since the age of 12, most readers do not substantially improve their efficiency and never reach their full capacity. Every computer-user who is also a slow typist is aware of the benefits he could obtain with a typing course, but nearly no one suspects the much higher profits he could reach by improving his reading comprehension and speed. The rapid improvement of voice recognition may gradually make typing virtuosity obsolete since a good typist performs well under the speed of speech. On the other hand, human or computer speaking, with an average speed of 150 wpm, will always remain many times slower than a good reader, without any consideration of the skimming and skipping possibilities.`;
+const initialText = `Hi, welcome to spreedy, if you haven't already, click play. This is an app I made when I was revising for my exams. I wanted to see if I could make reading more efficient, and I think this does just that. It doesn't only help slow readers double or even triple their reading speed, but has been tested to work with dyslexia and ADHD too. So here's how it works. At the bottom of your screen you can paste the text you would like to read. Then it's as simple as clicking the play button. If you want to adjust your speed, just move the slider along to change your WPM. If you want to go back, just pause and click on the word you want to start again from. For more information, click our logo at the top of your screen. That's it. I hope you like it.`;
 
 const scrollToCenter = function (id) {
   document.getElementById(id).scrollIntoView({
@@ -12,7 +16,6 @@ const scrollToCenter = function (id) {
     block: "center",
     inline: "center",
   });
-  console.log("scrolling to ", id);
 };
 
 const timers = [];
@@ -20,10 +23,14 @@ const timers = [];
 function App() {
   const [wordIdx, setwordIdx] = useState(0);
   const [paused, setPaused] = useState(true);
-  const [wpm, setWpm] = useState(400);
+  const [wpm, setWpm] = useState(350);
+  const [text, setText] = useState(initialText);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   useEffect(() => {
-    console.log("bruh");
     scrollToCenter("center");
   }, []);
 
@@ -35,28 +42,51 @@ function App() {
       timers.push(
         setTimeout(() => {
           setwordIdx(wordIdx + 1);
-          console.log(wordIdx);
           scrollToCenter("center");
-        }, 60000 / wpm + (text.split(" ")[wordIdx].slice(-1) === "." ? wpm / 4 : 0)) //
+        }, 60000 / wpm + (text.split(" ")[wordIdx].slice(-1) === "." ? 60000 / wpm : 0)) //
       );
-      // setExtraTime(0);
     }
   }
 
   return (
-    <div style={{ margin: "0 auto", width: "90vw", marginTop: "1rem" }}>
+    <div
+      style={{
+        margin: "0 auto",
+        width: "90vw",
+        marginTop: "1rem",
+        height: "auto",
+      }}
+    >
+      <Navbar>
+        <Navbar.Brand
+          onClick={handleShow}
+          className="navbar-brand mx-auto"
+          style={{ fontWeight: "bold" }}
+        >
+          <h1>spreedy</h1>
+        </Navbar.Brand>
+      </Navbar>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          For future enquires, email daniel@spreedy.app
+        </Modal.Footer>
+      </Modal>
       <div style={{ width: "100%", margin: "0 auto", textAlign: "center" }}>
         <Card
           style={{
             // width: "50rem",
             fontSize: "2rem",
-            height: "80vh",
+            height: "60vh",
             overflowX: "hidden",
             overflowY: "auto",
           }}
         >
           <Card.Body>
-            {[...Array(8).keys()].map((n) => {
+            {[...Array(9).keys()].map((n) => {
               return <br key={n} />;
             })}
             <div className={paused ? "shown" : "hidden"} id="before">
@@ -71,7 +101,6 @@ function App() {
                         console.log(i);
                         setwordIdx(i);
                         setTimeout(() => scrollToCenter(i), 0);
-                        // setImmediate(() => scrollToCenter(i));
                       }}
                       id={i}
                       key={i}
@@ -80,7 +109,15 @@ function App() {
                 })}
             </div>
             <div id="center">
-              <span id={wordIdx}>
+              <span
+                onClick={() => {
+                  for (let i = 0; i < timers.length; i++) {
+                    clearTimeout(timers[i]);
+                  }
+                  setPaused(paused ? false : true);
+                }}
+                id={wordIdx}
+              >
                 {text.split(" ").slice(wordIdx, wordIdx + 1)}
               </span>
             </div>
@@ -105,33 +142,82 @@ function App() {
                   );
                 })}
             </div>
-            {[...Array(8).keys()].map((n) => {
+            {[...Array(9).keys()].map((n) => {
               return <br key={n} />;
             })}
           </Card.Body>
         </Card>
 
         <Button
-          variant="secondary"
+          style={{ margin: "2rem" }}
+          variant="light"
           onClick={() => {
-            for (let i = 0; i < timers.length; i++) {
-              clearTimeout(timers[i]);
+            if ((wordIdx === text.split(" ").length - 1) & paused) {
+              setwordIdx(0);
+              setTimeout(() => scrollToCenter(0), 0);
+            } else {
+              for (let i = 0; i < timers.length; i++) {
+                clearTimeout(timers[i]);
+              }
+              setPaused(paused ? false : true);
             }
-            setPaused(paused ? false : true);
           }}
         >
-          {paused ? "▶️" : "⏸️"}
+          {/* {paused ? "▶️" : "⏸️"} */}
+          {paused ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              fill="currentColor"
+              className="bi bi-play"
+              viewBox="0 0 16 16"
+            >
+              <path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z" />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              fill="currentColor"
+              className="bi bi-pause"
+              viewBox="0 0 16 16"
+            >
+              <path d="M6 3.5a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5zm4 0a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5z" />
+            </svg>
+          )}
         </Button>
       </div>
 
       <RangeSlider
-        size=""
+        variant="secondary"
         min={60}
         max={800}
         value={wpm}
-        onChange={(e) => setWpm(e.target.value)}
+        onChange={(e) => {
+          for (let i = 0; i < timers.length; i++) {
+            clearTimeout(timers[i]);
+          }
+          setWpm(e.target.value);
+        }}
       />
-      {/* <Button onClick={() => scrollToCenter(4)}>Go to and</Button> */}
+      <Form
+        style={{ marginBottom: "2rem" }}
+        onSubmit={(e) => {
+          e.preventDefault();
+          for (let i = 0; i < timers.length; i++) {
+            clearTimeout(timers[i]);
+          }
+          setText(e.target[0].value);
+          setwordIdx(0);
+        }}
+      >
+        <FormControl
+          placeholder="Paste the text you want to read in here"
+          style={{ textAlign: "center" }}
+        ></FormControl>
+      </Form>
     </div>
   );
 }
